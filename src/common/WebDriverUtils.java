7a7f9c3e4067bd78bc7641cb33a9508a310806dc
Stepanov, Iaroslav <iaroslav.stepanov@t-systems.ru>
2017-12-02 16:19:11 +0300

    [+] browser setup methods

---
+++ src/common/WebDriverUtils.java
@@ -0,0 +1,37 @@
+package common;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class WebDriverUtils {
+    private static WebDriver driver;
+    private static final String CHROME_WEB_DRIVER_PATH = "Drivers\\chromedriver.exe";
+
+    private static WebDriver getChromeWebDriverPath() {
+        File file = new File(CHROME_WEB_DRIVER_PATH);
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        return new ChromeDriver();
+    }
+
+    public static void setDefaultZoom(WebDriver driver) {
+        Actions action = new Actions(driver);
+        action.sendKeys(Keys.chord(Keys.CONTROL, "0")).perform();
+    }
+
+    public static void setupBrowserWaitWindowAndCookies(WebDriver driver) {
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        setDefaultZoom(driver);
+    }
+
+    public static WebDriver launchChromeDriver() {
+        driver = getChromeWebDriverPath();
+        setupBrowserWaitWindowAndCookies(driver);
+        return driver;
+    }
+
+}
